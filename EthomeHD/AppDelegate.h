a330aa470d30862a1c159a34c83e13b7700a7d49
ethome <ethome@ethomedeimac.local>
2017-02-21 14:11:16 +0800

    Initial Commit

---
+++ EthomeHD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EthomeHD
+//
+//  Created by ethome on 2017/2/21.
+//  Copyright © 2017年 ethome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EthomeHD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EthomeHD
+//
+//  Created by ethome on 2017/2/21.
+//  Copyright © 2017年 ethome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
